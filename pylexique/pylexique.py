3283cfdce812b9e9d0597932b261ed0e535e3dac
SekouD <sekoud.python@gmail.com>
2018-07-21 23:36:16 +0200

    Added slots + formatting of fields.

--- pylexique/pylexique.py
+++ pylexique/pylexique.py
@@ -13,7 +13,7 @@ LEXIQUE382_PATH = '/'.join(('Lexique382', 'lexique382.pickle'))
 
 LEXIQUE382_FIELD_NAMES = ['ortho', 'phon', 'lemme', 'cgram', 'genre', 'nombre', 'freqlemfilms2', 'freqlemlivres', 'freqfilms2',
                           'freqlivres', 'infover', 'nbhomogr', 'nbhomoph', 'islem', 'nblettres', 'nbphons', 'cvcv', 'p_cvcv',
-                          'voisorth', 'voisphon', 'puorth', 'puphon', 'syll', 'nbsyll', 'cv-cv', 'orthrenv', 'phonrenv',
+                          'voisorth', 'voisphon', 'puorth', 'puphon', 'syll', 'nbsyll', 'cv_cv', 'orthrenv', 'phonrenv',
                           'orthosyll', 'cgramortho', 'deflem', 'defobs', 'old20', 'pld20', 'morphoder', 'nbmorph']
 
 
@@ -67,6 +67,8 @@ class Lexique382(object):
             fields = [field.split('_')[-1] for field in content[0].strip().split('\t')]
             if fields[17] == 'cvcv':
                 fields[17] = 'p_cvcv'
+            if fields[24] == 'cv-cv':
+                fields[17] = 'cv_cv'
             lexique382_dict = defaultdict(list)
             for row in content[1:]:
                 row_fields = row.strip().split('\t')
@@ -80,6 +82,8 @@ class LexEntry(object):
 
     :param row_fields:
     """
+    __slots__ = LEXIQUE382_FIELD_NAMES
+
     def __init__(self, row_fields):
         for attr, value in zip(LEXIQUE382_FIELD_NAMES, row_fields):
             setattr(self, attr, value)
