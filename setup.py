c0d00428cf0091604a1247d45f1ad38cbc6d05a0
SekouDiaoNlp <diao.sekou.nlp@gmail.com>
2021-04-26 16:57:45 +0200

    Bump version: 0.1.0 → 1.0.0

--- setup.py
+++ setup.py
@@ -63,6 +63,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/SekouDiaoNlp/pylexique',
-    version='0.1.0',
+    version='1.0.0',
     zip_safe=False,
 )
