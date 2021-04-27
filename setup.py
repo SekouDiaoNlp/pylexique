85c895ed0bf9562fd1377e84152babca19b72ddc
Sekou Diao <52342703+SekouDiaoNlp@users.noreply.github.com>
2021-04-27 13:13:37 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['pytables', 'cython', 'numexpr', 'numpy', 'blosc', 'hf5py', 'Click>=7.1', ]
+requirements = ['tables', 'cython', 'numexpr', 'numpy', 'blosc', 'hf5py', 'Click>=7.1', ]
 
 setup_requirements = ['pytest-runner', ]
 
