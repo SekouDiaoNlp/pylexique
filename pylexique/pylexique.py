2843e00f43ed6bfe68afc27554fbf4a8c595e1cc
SekouDiaoNlp <diao.sekou.nlp@gmail.com>
2021-05-06 04:27:52 +0200

    new import style.

--- pylexique/pylexique.py
+++ pylexique/pylexique.py
@@ -10,7 +10,7 @@ import json
 from dataclasses import dataclass
 from typing import List, Optional, Tuple, Union, ClassVar
 from time import time
-from pylexique.utils import logger
+from utils import logger
 
 _RESOURCE_PACKAGE = __name__
 
