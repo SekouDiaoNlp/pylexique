2031b48b594be5a369a5c14f6e031bfe530ab9c5
Sekou Diao <diao.sekou.nlp@gmail.com>
2021-05-09 21:34:32 +0200

    Update pylexique.py
    Uses pandas for now

--- pylexique/pylexique.py
+++ pylexique/pylexique.py
@@ -6,6 +6,7 @@ from collections import OrderedDict, defaultdict
 from collections.abc import Sequence
 import pkg_resources
 import json
+import pandas as pd
 # import faster_than_csv as csv
 from dataclasses import dataclass
 from time import time
@@ -122,22 +123,28 @@ class Lexique383:
     value_errors = []
     length_errors = []
     lemmes = defaultdict(list)
+    row_fields = []
 
     def __init__(self, lexique_path: Optional[str] = None) -> None:
         self.lexique_path = lexique_path
         if lexique_path:
-            t0 = time()
-            self._parse_lexique(self.lexique_path)
-            t1 = round(time() - t0, 2)
-        else:
             try:
                 # Tries to load the pre-shipped Lexique38X if no path file to the lexicon is provided.
-                self._parse_lexique(_RESOURCE_PATH)
+                self._parse_lexique(self.lexique_path)
             except FileNotFoundError:
                 if isinstance(lexique_path, str):
                     raise ValueError(f"Argument 'lexique_path' must be a valid path to Lexique383")
                 if not isinstance(lexique_path, str):
                     raise TypeError(f"Argument 'lexique_path'must be of type String, not {type(lexique_path)}")
+        else:
+            try:
+                # Tries to load the pre-shipped Lexique38X if no path file to the lexicon is provided.
+                self._parse_lexique(_RESOURCE_PATH)
+            except FileNotFoundError:
+                if isinstance(_RESOURCE_PATH, str):
+                    raise ValueError(f"Argument 'lexique_path' must be a valid path to Lexique383")
+                if not isinstance(_RESOURCE_PATH, str):
+                    raise TypeError(f"Argument 'lexique_path'must be of type String, not {type(_RESOURCE_PATH)}")
         return
 
     def __repr__(self):
@@ -153,14 +160,16 @@ class Lexique383:
         :param lexique_path: string.
             Path to the lexique csv file.
         :return:
-        """
-        with open(lexique_path, 'r', encoding='utf-8', errors='ignore') as csv_file:
-            content = csv_file.readlines()
-            self._create_db(content)
-            if self.value_errors:
-                self._save_errors(self.value_errors, _VALUE_ERRORS_PATH)
-            if self.length_errors:
-                self._save_errors(self.length_errors, _LENGTH_ERRORS_PATH)
+        """        
+        # Create a dataframe from csv
+        df = pd.read_csv(lexique_path, delimiter=',')
+        self.row_fields = df.columns.to_list()
+        content = (list(row) for row in df.values)
+        self._create_db(content)
+        if self.value_errors:
+            self._save_errors(self.value_errors, _VALUE_ERRORS_PATH)
+        if self.length_errors:
+            self._save_errors(self.length_errors, _LENGTH_ERRORS_PATH)
         return
 
     def _create_db(self, lexicon: List[str]) -> None:
@@ -172,21 +181,20 @@ class Lexique383:
             Iterable containing the lexique383 entries.
         :return:
         """
-        for i, row in enumerate(lexicon[1:]):
-            row_fields = row.strip().split('\t')
+        for i, row in enumerate(lexicon):
             try:
-                row_fields = self._convert_entries(row_fields)
+                self.row_fields = self._convert_entries(row_fields)
             except ValueError as e:
                 continue
-            lexical_entry = LexItem(*row_fields)
+            lexical_entry = LexItem(*self.row_fields)
             self.lemmes[lexical_entry.lemme].append(lexical_entry)
-            if row_fields[0] in self.lexique and not isinstance(self.lexique[row_fields[0]], list):
-                self.lexique[row_fields[0]] = [self.lexique[row_fields[0]]]
-                self.lexique[row_fields[0]].append(lexical_entry)
-            elif row_fields[0] in self.lexique and isinstance(self.lexique[row_fields[0]], list):
-                self.lexique[row_fields[0]].append(lexical_entry)
+            if self.row_fields[0] in self.lexique and not isinstance(self.lexique[self.row_fields[0]], list):
+                self.lexique[self.row_fields[0]] = [self.lexique[self.row_fields[0]]]
+                self.lexique[self.row_fields[0]].append(self.lexical_entry)
+            elif self.row_fields[0] in self.lexique and isinstance(self.lexique[self.row_fields[0]], list):
+                self.lexique[self.row_fields[0]].append(lexical_entry)
             else:
-                self.lexique[row_fields[0]] = lexical_entry
+                self.lexique[self.row_fields[0]] = lexical_entry
         return
 
     def _convert_entries(self, row_fields: List[str]) -> List[Union[str, float, int, bool]]:
