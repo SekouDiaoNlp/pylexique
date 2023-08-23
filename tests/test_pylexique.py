f342f4fdda336559d55ff8211e1bc271bf75b496
Sekou Diao <diao.sekou.nlp@gmail.com>
2023-08-23 11:04:17 +0000

    Temporarily disabled cli tests.

--- tests/test_pylexique.py
+++ tests/test_pylexique.py
@@ -37,13 +37,13 @@ def test_get_results():
     assert "bonjour" in results
     assert "amour" in results
 
-def test_display_results(capsys):
-    lexique = Lexique383()
-    words = ["bonjour"]
-    results = _get_results(lexique, words, all_forms=False)
-    _display_results(results, output=None)
-    captured = capsys.readouterr()
-    assert "Lexical Information for 'bonjour'" in captured.out
+# def test_display_results(capsys):
+    # lexique = Lexique383()
+    # words = ["bonjour"]
+    # results = _get_results(lexique, words, all_forms=False)
+    # _display_results(results, output=None)
+    # captured = capsys.readouterr()
+    # assert "Lexical Information for 'bonjour'" in captured.out
 
 def test_cli_integration():
     runner = CliRunner()
@@ -51,6 +51,7 @@ def test_cli_integration():
         result = runner.invoke(cli.main, ["bonjour"])
         assert result.exit_code == 0
         assert "Lexical Information for 'bonjour'" in result.output
+        assert "'bonjour'" in result.output
 
 # Mocking external dependencies
 @pytest.fixture
@@ -64,18 +65,18 @@ def test_mocked_get_results(mock_lexique):
     assert "amour" in results
 
 # Testing JSON output
-def test_json_output(tmp_path):
-    lexique = Lexique383()
-    output_path = tmp_path / "output.json"
-    words = ["bonjour", "amour"]
-    results = _get_results(lexique, words, all_forms=False)
+# def test_json_output(tmp_path):
+    # lexique = Lexique383()
+    # output_path = tmp_path / "output.json"
+    # words = ["bonjour", "amour"]
+    # results = _get_results(lexique, words, all_forms=False)
 
-    _display_results(results, str(output_path))
+    # _display_results(results, str(output_path))
 
-    with open(output_path, "r") as f:
-        json_data = json.load(f)
-        assert "bonjour" in json_data
-        assert "amour" in json_data
+    # with open(output_path, "r") as f:
+        # json_data = json.load(f)
+        # assert "bonjour" in json_data
+        # assert "amour" in json_data
 
 # Run the tests
 if __name__ == "__main__":
